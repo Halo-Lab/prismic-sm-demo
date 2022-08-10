d6d44d1bd7563429c6d5d9fa135387eaf007200e
irinainina <irbris@mail.ru>
2022-08-10 15:05:48 +0300

    feat: update SM Preview

--- pages/slice-simulator.jsx
+++ pages/slice-simulator.jsx
@@ -5,10 +5,12 @@ import state from "../.slicemachine/libraries-state.json";
 import { components } from "../slices";
 
 const SliceSimulatorPage = () => (
-<SliceSimulator
-  sliceZone={(props) => <SliceZone {...props} components={components} />}
-  state={state}
-/>);
+  <SliceSimulator
+    // The "sliceZone" prop should be a function receiving slices and rendering them using your "SliceZone" component.
+    sliceZone={(props) => <SliceZone {...props} components={components} />}
+    state={state}
+  />
+);
 
 export default SliceSimulatorPage;
 
