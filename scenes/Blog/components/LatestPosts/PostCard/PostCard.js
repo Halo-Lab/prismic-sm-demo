c2afda55a1c055e56b547023279016aaf8b76d27
MityaDementiy <d.dementiy@gmail.com>
2022-11-09 11:18:22 +0200

    Upgrade Link in scenes dir

--- scenes/Blog/components/LatestPosts/PostCard/PostCard.js
+++ scenes/Blog/components/LatestPosts/PostCard/PostCard.js
@@ -15,7 +15,6 @@ const PostCard = ({ post, index, isOpenPosts }) => {
       key={index}
     >
       <Link href={`/blog/${contentrelationship.uid}`} className={styles.postCard}>
-
         <div className={styles.postImageBox}>
           <div className={styles.imageContainer}>
             <Image src={postimage.url} className={styles.image} alt={postimage.alt} layout="fill" priority /> 
@@ -25,7 +24,6 @@ const PostCard = ({ post, index, isOpenPosts }) => {
         <p className={styles.postDescription}>
           {cutDescription(posttext[0].text, 87)}
         </p>
-
       </Link>
     </div>
   );
